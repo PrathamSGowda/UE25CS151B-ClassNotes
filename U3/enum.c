b762c7316721b7acfe5c61d416237e573c0a461f
Pratham S Gowda <prathampsg2007@gmail.com>
2026-04-22 13:16:03 +0530

    Enum

---
+++ U3/enum.c
@@ -0,0 +1,24 @@
+// enum are not identifiers and therefore no memory is allocated
+// enum assigns 0 to first member and increments it by 1 for the next members
+#include <stdio.h>
+enum days
+{
+    // mon,tue,wed,thu,fri,sat,sun
+    mon=1,tue,wed,thu,fri,sat,sun // tue = 2 wed = 3 and so on
+};
+int main()
+{
+    // printf("%d\n",sizeof(enum days)); // 4 because integral constant
+    // printf("%d\n",mon);
+    // for(int i=mon;i<sun;i++)
+    // {
+    //     printf("%d\n",i);
+    // }
+    enum days d;
+    printf("%d %d\n",d,sizeof(d));
+    // d.mon = 1; // error because mon is a constant and we are trying to change the value of it
+    // to change the value we should assign it in enum declaration 
+    d = mon; // d is initialised with a integer constant monday
+    printf("%d\n",d);
+    return 0;
+}
