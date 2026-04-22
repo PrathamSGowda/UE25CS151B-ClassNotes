1b8d4521699eafb864aa655b2b5aa07ee10e8433
Pratham S Gowda <prathampsg2007@gmail.com>
2026-04-22 12:39:03 +0530

    Union

--- U3/union.c
+++ U3/union.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+/*
 struct sample
 {
     int a;
@@ -23,5 +24,51 @@ int main()
     d.c = 67;
     printf("%f\n",d.c);
     // printf("%d %c %f\n",d.a,d.b,d.c); // corrupted output
+}
+*/
 
+// union inside union
+/*
+union demo
+{
+    int i;
+    union
+    {
+        char k;
+        float j;
+    }u;
+};
+int main()
+{
+    printf("%d\n",sizeof(union demo));
+    union demo d;
+    printf("Enter i\n");
+    scanf("%d",&d.i);
+    printf("%d\n",d.i);
+    d.u.k = 'C';
+    printf("%c\n",d.u.k);
+    d.u.j = 56.34;
+    printf("%f\n",d.u.j);
+    // printf("%d\n",d.u.i); // error as i is member of outer union
+}
+*/
+
+union demo
+{
+    int i;
+    union sample
+    {
+        char k;
+        float j;
+    };
+};
+int main()
+{
+    printf("%d\n",sizeof(union demo));
+    printf("%d\n",sizeof(union sample));
+    union sample s1;
+    s1.k = 'D';
+    printf("%c\n",s1.k);
+    s1.j = 22.23;
+    printf("%f\n",s1.j);
 }
